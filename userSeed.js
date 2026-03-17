7c63bf873f380fe2e8f44e9c795e999a0e52ab54
Kernyuy Patrick <https://github.com/BoyK27>
2026-03-17 07:07:19 +0100

    corected

--- userSeed.js
+++ userSeed.js
@@ -1,22 +1,30 @@
 import User from "./models/User.js";
 import bcrypt from "bcrypt";
 import connectToDatabase from "./db/db.js";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
 const userRegister = async () => {
-  connectToDatabase();
+  await connectToDatabase();
+
   try {
-    const hashPassword = await bcrypt.hash("glaine", 10);
+    const hashPassword = await bcrypt.hash("glaine#Man", 10);
     const newUser = new User({
       name: "Admin",
       email: "brodev@gmail.com",
       password: hashPassword,
       role: "admin",
     });
+
     await newUser.save();
+    console.log("Admin user created successfully in Atlas!");
   } catch (error) {
-    console.log(error);
+    console.log("Error seeding database:", error);
+  } finally {
+    mongoose.connection.close();
+    console.log("Connection closed.");
+    process.exit(0);
   }
 };
 
