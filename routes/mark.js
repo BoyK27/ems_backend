8ca1164994f44f51ecfc7fbd6c3c841d8ebe4be5
kernyuypatrick702@gmail.com <kernyuypatrick702@gmail.com>
2026-07-31 04:35:30 +0100

    student marks and filling attempt 7

--- routes/mark.js
+++ routes/mark.js
@@ -1,15 +1,20 @@
+import express from "express";
 import {
   submitOrUpdateMarks,
   getMarksByClassAndSubject,
   getStudentMarks,
 } from "../controllers/markController.js";
-
 import authMiddleware from "../middleware/authmiddleware.js";
 
 const router = express.Router();
 
+// Save or edit marks
 router.post("/save", authMiddleware, submitOrUpdateMarks);
+
+// Fetch class/subject marks using Query Params (?classId=...&subjectId=...&examSessionId=...)
 router.get("/class-subject", authMiddleware, getMarksByClassAndSubject);
+
+// Express 5 compatible routes for student marks
 router.get("/student/:studentId", authMiddleware, getStudentMarks);
 router.get(
   "/student/:studentId/:examSessionId",
