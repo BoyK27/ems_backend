48b1952d62f68b1f6a5e37ff04c77caa138df57d
kernyuypatrick702@gmail.com <kernyuypatrick702@gmail.com>
2026-07-31 04:23:26 +0100

    student marks and filling attempt 7

--- routes/mark.js
+++ routes/mark.js
@@ -1,24 +1,16 @@
-import express from "express";
 import {
-  saveMarks,
-  getMarksForClassSubject,
+  submitOrUpdateMarks,
+  getMarksByClassAndSubject,
   getStudentMarks,
 } from "../controllers/markController.js";
+
 import authMiddleware from "../middleware/authmiddleware.js";
 
 const router = express.Router();
 
-// Route to save/update marks
-router.post("/save", authMiddleware, saveMarks);
-
-// Route to fetch marks for lecturer view
-router.get("/class-subject", authMiddleware, getMarksForClassSubject);
-
-// --- EXPRESS 5 FIX FOR OPTIONAL PARAMETERS ---
-// 1. Route WITHOUT optional examSessionId
+router.post("/save", authMiddleware, submitOrUpdateMarks);
+router.get("/class-subject", authMiddleware, getMarksByClassAndSubject);
 router.get("/student/:studentId", authMiddleware, getStudentMarks);
-
-// 2. Route WITH examSessionId
 router.get(
   "/student/:studentId/:examSessionId",
   authMiddleware,
