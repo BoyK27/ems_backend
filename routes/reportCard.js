2ab1308120e75d86b8479056eb7c0451c967a2ce
kernyuypatrick702@gmail.com <kernyuypatrick702@gmail.com>
2026-08-25 12:21:59 +0100

    Report card fixs three

--- routes/reportCard.js
+++ routes/reportCard.js
@@ -5,32 +5,20 @@ import {
   togglePublishReportCards,
 } from "../controllers/reportCardController.js";
 import verifyUser from "../middleware/authmiddleware.js";
-import verifyRole from "../middleware/verifyRole.js";
 
 const router = express.Router();
 
-// Class Leaderboard (Admin only)
-router.get(
-  "/class-summary/:semesterId",
-  verifyUser,
-  verifyRole(["admin"]),
-  getClassReportCardSummary,
-);
+// Class Leaderboard
+router.get("/class-summary/:semesterId", verifyUser, getClassReportCardSummary);
 
-// Individual Student Report Card (Admins & Students)
+// Individual Student Report Card
 router.get(
   "/student/:studentId/semester/:semesterId",
   verifyUser,
-  verifyRole(["admin", "student"]),
   getSemesterReportCard,
 );
 
-// Toggle publish state (Admin only)
-router.put(
-  "/publish/:semesterId",
-  verifyUser,
-  verifyRole(["admin"]),
-  togglePublishReportCards,
-);
+// Toggle publish state
+router.put("/publish/:semesterId", verifyUser, togglePublishReportCards);
 
 export default router;
